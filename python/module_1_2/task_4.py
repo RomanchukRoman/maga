234309a8e996748467d67b03f4b968a70ed995c8
RomanchukRoman <r.romanchuk@ya.ru>
2025-09-10 23:49:33 +0300

    add task 4

---
+++ python/module_1_2/task_4.py
@@ -0,0 +1,24 @@
+def calculate_bmi(weight, height):
+    return weight / (height ** 2)
+
+def get_bmi_category(bmi):
+    if bmi < 25:
+        return 'Норма, жить можно'
+    elif bmi >= 25 and bmi <= 30:
+        return 'Избыточный вес, стоит задуматься'
+    else:
+        return 'Ожирение'
+
+weight = float(input('Введите ваш вес (кг): '))
+height = float(input('Введите ваш рост (м): '))
+
+if weight < 0:
+    print('Вес не может быть отрицательными')
+elif height < 0:
+    print('Рост не может быть отрицательными')
+else:
+    bmi = calculate_bmi(weight, height)
+    category = get_bmi_category(bmi)
+
+    print("Ваш ИМТ: ", round(bmi, 1))
+    print("Категория: ", category)
