9a80e0f8d9999578ddfbf965152205049b5a171c
RomanchukRoman <r.romanchuk@ya.ru>
2025-09-10 22:14:57 +0300

    change folders

---
+++ python/module_1_2/example.py
@@ -0,0 +1,20 @@
+def calculate_sum(a, b):
+ c = a + b
+ d = 10  # Неиспользуемая переменная
+ print(c)
+ print(e)  # Неопределенная переменная
+
+import math  # Неиспользуемый импорт
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+def another_function():
+   pass  # Неиспользуемая функция
+
+print('Hello, World!')
+
+# ruff check example.py проверка синтаксиса
+# подробнее тут https://practicum.yandex.ru/learn/higher-data-analytics/courses/cd885fc5-65a3-4276-9eef-924a976d6c4d/sprints/730469/topics/380197db-9aa1-4a6f-9e53-d473cffb7428/lessons/ef1eb4cf-ddb0-42fd-9c1b-113597d51dc0/
